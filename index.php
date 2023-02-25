af0870695eeb1b74d87fe676f21e7861ba6af5c7
Carlos Carballo <ballokalitos@gmail.com>
2023-02-25 14:17:57 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,157 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">        <!-- Para mostrar acentos y letras como ñ -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Ballo">
+
+    <title>Michis Comida para gatos</title>
+    
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/heroic-features.css" rel="stylesheet">
+
+    <link rel="icon" href="images/logosCat.png">
+
+    <link rel="stylesheet" href="style.css">
+    <script src="main.js"></script>
+  </head>
+
+  <body>
+    <link href="css/heroic-features.css" rel="stylesheet">
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <img src="images/logosCat.png" style="height:5%;width: 5%;" title="mycompanylogo" class="logos">
+        
+		      <!-- Títulos y cabeceras de página -->
+				
+		<a class="navbar-brand" href="#" >Michis Comida para gatos</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            
+			 <!-- Listas para mostrar menu -->
+			 
+			<li class="nav-item active">
+              <a class="nav-link" href="web-index.html">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="web-about.html">About</a>
+            </li>
+            
+          
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+    
+      <style type="text/css">
+      
+      .jumbotron{
+          background-image: url("images/cat-640.png");
+          background-size: cover;
+        }
+      </style>
+      
+      <header class="jumbotron my-4">
+        <h1 class="display-3">Bienvenidos a Michis comida para gatos Sitio Prueba desde Openshift!</h1>
+        <p class="lead"> esta es un web de Prueba de comida para gatos</p>
+        <a href="https://www.petco.com.mx/petco/en/PRODUCTOS/Gato/Alimento/c/02-01-00?siteName=Petco+Mexico&siteUid=petco" class="btn btn-primary btn-lg">Iniciar Compra!</a>
+              <!-- Hipervínculos -->
+	  </header>
+
+      <!-- 
+             Uso de Encabezados <h3>  y Parrafos  <p>
+	  -->
+      <h3>Lista Productos:</h3><br><br>
+      <div class="row text-center">
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card">
+            <img class="card-img-top" src="images/productos/purinaCat.png" alt="">
+			
+			        	<!-- Imágenes-->
+			
+			
+            <div class="card-body">
+              <h4 class="card-title">Cuidado urinario</h4>
+              <p class="card-text">PURINA ONE® CUIDADO URINARIO está específicamente diseñado para ayudar a mantener un sistema urinario saludable. </p>
+            </div>
+            
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card">
+            <img class="card-img-top" src="images/productos/merrickp.jpg" alt="">
+            <div class="card-body">
+              <h4 class="card-title">Merrick Purrfect Bistro</h4>
+              <p class="card-text">Alimento Natural sin Granos para Gato Adulto Receta Salmón, 5.4 kg</p>
+            </div>
+            
+          </div>
+        </div>
+<!-- No podrá ver este texto. 
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card">
+            <img class="card-img-top" src="images/watches/watch3.png" alt="">
+            <div class="card-body">
+              <h4 class="card-title">Rolex Oyster Perpetual</h4>
+              <p class="card-text">Stainless steel case with a stainless steel Rolex jubilee bracelet. Fixed fluted 18kt white gold bezel. Blue jubilee dial with silver-tone hands and diamond hour markers.</p>
+            </div>
+            
+          </div>
+        </div>
+-->
+
+<!-- No podrá ver este texto.
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card">
+            <img class="card-img-top" src="images/watches/watch4.png" alt="">
+            <div class="card-body">
+              <h4 class="card-title">Rolex Perpetual Datejust Pink</h4>
+              <p class="card-text">Stainless steel case with a stainless steel Rolex jubilee bracelet with 18kt everose gold center links. Fixed fluted 18kt everose gold bezel.</p>
+            </div>
+           
+          </div>
+        </div>
+-->
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+        <div class="container">
+          <p class="m-0 text-center text-white">Copyright &copy; BalloKalitos2023_Maestria_Unid</p>
+        </div>
+        <!-- /.container -->
+      </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
+
